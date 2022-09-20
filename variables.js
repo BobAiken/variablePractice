16b8643386e2ebe8ce5da4a021288a39c3197beb
Robert Aiken <robertsiraaiken@gmail.com>
2022-09-20 14:36:12 -0500

    Add reassigned strings

--- variables.js
+++ variables.js
@@ -4,7 +4,12 @@ var numbers = "1337"
 var realNumbers = 1972
 var otherNumbers = 991234
 var moreNumbers = 88222111444
-var depositPaid = True
-var isBoolean = True
-var isFalse = False
- 
+var depositPaid = true
+var isBoolean = true
+var isFalse = false
+var email = "worldHasEnded@email"
+var name = "Chuck"
+var numbers = "111111"
+console.log(email)
+console.log(name)
+console.log(numbers)
